08432d53dfeb8ca9b4be9cf434fcb73928f92734
Simon Suska <bvbnomis@icloud.com>
2024-03-08 18:16:45 +0100

    #4 - implement Scope

---
+++ Tests/InjectleTests/ScopeTests/ScopeTestObjects.swift
@@ -0,0 +1,18 @@
+import Foundation
+import Injectle
+
+class ScopeTestClass: NSCopying {
+    private var value: Int
+    
+    init(value: Int) {
+        self.value = value
+    }
+    
+    func someMethod() -> Int {
+        value
+    }
+    
+    func copy(with zone: NSZone? = nil) -> Any {
+        return ScopeTestClass(value: self.value)
+    }
+}
--- Tests/InjectleTests/ScopeTests/TestObjects.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import Injectle
-
-class ScopeTestClass {
-    private var value: Int
-    init(value: Int) { self.value = value }
-    func someMethod() -> Int { value }
-}
